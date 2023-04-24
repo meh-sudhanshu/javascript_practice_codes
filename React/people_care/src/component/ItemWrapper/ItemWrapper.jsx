715ddc4397142e52fefe41c20733ce1c0b14bc75
sudhanshu kumar <sudhanshukumar8816@gmail.com>
2023-04-24 21:06:38 +0530

    class codes

--- React/people_care/src/component/ItemWrapper/ItemWrapper.jsx
+++ React/people_care/src/component/ItemWrapper/ItemWrapper.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import './itemwrapper.css'
+import MessageBox from "../MessageBox/MessageBox";
 
 
 const ItemWrapper = (props)=>{
@@ -9,12 +10,18 @@ const ItemWrapper = (props)=>{
         backgroundColor:props.item.backgroundColor
     }
 
+    const [flag , setFlag] = useState(false)
+    const buttonHandler = ()=>{
+        setFlag(true)
+    }
+
     return(
         <div className="item_wrapper" style={style}>
+            {flag && <MessageBox/>}
             <h2 className="id_">{props.item.id}</h2>
             <h2 className="doc_name">{props.item.name}</h2>
             <h2 className="doc_speciality">{props.item.speciality}</h2>
-            <button className="message_btn">Message</button>
+            <button className="message_btn" onClick={buttonHandler}>Message</button>
             <button className="appointment_btn">Book an appointment</button>
         </div>
     )
---
+++ React/people_care/src/component/MessageBox/MessageBox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './message_box.css'
+
+
+const MessageBox = ()=>{
+    return(
+        <div className="message_box">
+            
+        </div>
+    )
+}
+
+
+
+export default MessageBox
